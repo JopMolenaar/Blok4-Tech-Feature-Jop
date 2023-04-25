793fa118c3b193675f83b853e230988df1b9f548
Jop <jopmolenaar@icloud.com>
2023-04-25 09:52:13 +0200

    extra routes

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const name = require("./js/name.js")
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send(`Hello World ${name()} `)
+app
+  .use(`/static`, express.static(`static`))
+  .get('/', function (req, res) {
+    res.send(`Hello World ${name()} `)
+  })
+
+app.get('/about', function (req, res) {
+  res.send(`About ${name()} `)
+})
+
+app.get('/login', function (req, res) {
+  res.send(`Log in ${name()} `)
 })
 
 app.listen(3000)
