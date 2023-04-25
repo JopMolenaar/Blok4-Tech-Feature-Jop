f06efcd6c0a0d62a42c8aab24c1f628157c3a4c9
Jop <jopmolenaar@icloud.com>
2023-04-25 11:57:26 +0200

    template is now working

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 const name = require("./js/name.js")
 const express = require('express')
 const app = express();
-const PORT = process.env.PORT || 1337
+const PORT = process.env.PORT || 3056
 
+const  { engine } = require("express-handlebars");
 
-const  { create } = require("express-handlebars");
-
-const hbs = create({ /* config */ });
-
-// Register `hbs.engine` with the Express app.
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './view');
+app.set('views', './views');
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
-app.get('/', function (req, res) {
-    // res.render(`index`)
-    res.send(`Hoi ${name()} `)
-  })
-
-app.get('/about', function (req, res) {
-  res.send(`About ${name()} `)
-})
-
-app.get('/login', function (req, res) {
-  res.send(`Log in ${name()} `)
-})
-
-
-
-app.get(`*`, function (req, res) {
+app.get(`*`, (req, res) => {
   res.status(404).send('Error 404');
 })
 
@@ -37,4 +21,21 @@ app.listen(PORT, () =>{
 console.log(`server running on port: ${PORT}`);
 })
 
-app.listen(3000)
+app.listen(3000);
+
+
+
+
+// app.get('/', function (req, res) {
+//     // res.send(`Hoi ${name()} `)
+//   })
+
+// app.get('/about', (req, res) => {
+//   res.send(`About ${name()} `)
+// })
+
+// app.get('/login', (req, res) => {
+//   res.send(`Log in ${name()} `)
+// })
+
+// app.listen(3000)
