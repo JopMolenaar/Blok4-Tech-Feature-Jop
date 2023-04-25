5193ce0a73c39ef661a79f6ca197904c06a69d82
Jop <jopmolenaar@icloud.com>
2023-04-25 16:07:10 +0200

    added js and css to handlebars

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-const name = require("./js/name.js")
+const name = require("./static/js/name.js")
 const express = require('express')
 const app = express();
-const PORT = process.env.PORT || 3056
+const PORT = process.env.PORT || 3000
+const path = require('path');
 
-const  { engine } = require("express-handlebars");
-
+const { engine } = require("express-handlebars");
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(express.static(path.join(__dirname, '/static')));
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home')
 });
+
+app.get('/about', (req, res) => {
+  res.send(`About ${name()} `)
+})
+
+app.get('/login', (req, res) => {
+  res.send(`Log in ${name()} `)
+})
 
 app.get(`*`, (req, res) => {
   res.status(404).send('Error 404');
@@ -21,21 +30,3 @@ app.listen(PORT, () =>{
 console.log(`server running on port: ${PORT}`);
 })
 
-app.listen(3000);
-
-
-
-
-// app.get('/', function (req, res) {
-//     // res.send(`Hoi ${name()} `)
-//   })
-
-// app.get('/about', (req, res) => {
-//   res.send(`About ${name()} `)
-// })
-
-// app.get('/login', (req, res) => {
-//   res.send(`Log in ${name()} `)
-// })
-
-// app.listen(3000)
