27603d0a83eeac4feb2ab9b8799bf275823445a5
Jop <jopmolenaar@icloud.com>
2023-05-08 14:17:12 +0200

    eslint fix

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const name = require('./static/js/name.js')
-const express = require('express')
+const name = require("./static/js/name.js")
+const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
-const path = require('path')
+const path = require("path")
 
-const testLink = 'Swipe area'
+const testLink = "Swipe area"
 
-const { engine } = require('express-handlebars')
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', './views')
+const { engine } = require("express-handlebars")
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", "./views")
 
-app.use(express.static(path.join(__dirname, '/static')))
+app.use(express.static(path.join(__dirname, "/static")))
 
-app.get('/', (req, res) => {
-    res.render('home', {linkOne: testLink})
+app.get("/", (req, res) => {
+    res.render("home", {linkOne: testLink})
 })
 
-app.get('/about', (req, res) => {
+app.get("/about", (req, res) => {
   res.send(`About ${name()}`)
 })
 
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   res.send(`Log in ${name()}`)
 })
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   // res.render('home')
-  res.status(404).render('notfound')
+  res.status(404).render("notfound")
 })
 
 app.listen(PORT, () =>{
