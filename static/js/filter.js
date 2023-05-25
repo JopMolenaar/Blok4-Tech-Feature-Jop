7de427a12acdf1e148b6a3a55f82438238b15f5c
Jop <jopmolenaar@icloud.com>
2023-05-25 10:18:01 +0200

    fixed some styling

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,43 +1,45 @@
-const body = document.querySelector("body")
-const filterNav = document.querySelector("#locations section:nth-child(3)")
-const locationsList = document.querySelector("#locations ul")
-const selects = filterNav.querySelectorAll("select")
-const jsOff = (document.getElementById("javascriptOff").style.display = "none")
-selects.forEach((select) => {
-    select.addEventListener("change", () => {
-        const others = locationsList.querySelectorAll(`li`)
-        if (select.value != 0) {
-            const answers = locationsList.querySelectorAll(`.${select.value}`)
-            others.forEach((item) => {
-                item.style.display = "none"
-            })
-            answers.forEach((item) => {
-                item.style.display = "grid"
-            })
-        } else {
-            others.forEach((item) => {
-                item.style.display = "grid"
-            })
-        }
+window.onload = () => {
+    const body = document.querySelector("body")
+    const filterNav = document.querySelector("#locations section:nth-child(3)")
+    const locationsList = document.querySelector("#locations ul")
+    const selects = filterNav.querySelectorAll("select")
+    const jsOff = (document.getElementById("javascriptOff").style.display = "none")
+    selects.forEach((select) => {
+        select.addEventListener("change", () => {
+            const others = locationsList.querySelectorAll(`li`)
+            if (select.value != 0) {
+                const answers = locationsList.querySelectorAll(`.${select.value}`)
+                others.forEach((item) => {
+                    item.style.display = "none"
+                })
+                answers.forEach((item) => {
+                    item.style.display = "grid"
+                })
+            } else {
+                others.forEach((item) => {
+                    item.style.display = "grid"
+                })
+            }
+        })
     })
-})
-let optionValueArray = []
-const fixOptions = () => {
-    const options = filterNav.querySelectorAll("option")
-    options.forEach((option) => {
-        if (optionValueArray.includes(`${option.value}`) || option.value === "") {
-            option.style.display = "none"
-        }
-        optionValueArray.push(`${option.value}`)
-    })
-}
+    let optionValueArray = []
+    const fixOptions = () => {
+        const options = filterNav.querySelectorAll("option")
+        options.forEach((option) => {
+            if (optionValueArray.includes(`${option.value}`) || option.value === "") {
+                option.style.display = "none"
+            }
+            optionValueArray.push(`${option.value}`)
+        })
+    }
 
-fixOptions()
-switch (body.id) {
-    case "home":
-    case "mainHandlebars":
-        break
-    default:
-        console.error("this id is not supported")
-        break
+    fixOptions()
+    switch (body.id) {
+        case "home":
+        case "mainHandlebars":
+            break
+        default:
+            console.error("this id is not supported")
+            break
+    }
 }
--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -1,38 +1,40 @@
-const countryFilled = document.getElementById("countryFilled")
-const cityFilled = document.getElementById("cityFilled")
-const AtFilled = document.getElementById("AtFilled")
-// const ipAdress = document.getElementById("ipAdress")
-const jsOff = (document.getElementById("javascriptOffAtAdd").style.display = "none")
-const country = document.getElementById("country")
-const city = document.getElementById("city")
-const park = document.getElementById("park")
+window.onload = () => {
+    const countryFilled = document.getElementById("countryFilled")
+    const cityFilled = document.getElementById("cityFilled")
+    const AtFilled = document.getElementById("AtFilled")
+    // const ipAdress = document.getElementById("ipAdress")
+    const jsOff = (document.getElementById("javascriptOffAtAdd").style.display = "none")
+    const country = document.getElementById("country")
+    const city = document.getElementById("city")
+    const park = document.getElementById("park")
 
-const findip = () => {
-    $.get("https://ipapi.co/" + "" + "/json", (data) => {
-        // document.getElementById("out").innerHTML = "ip: " + data.ip + "<br>Location " + data.city + "," + data.region + "," + data.country_name
+    const findip = () => {
+        $.get("https://ipapi.co/" + "" + "/json", (data) => {
+            // document.getElementById("out").innerHTML = "ip: " + data.ip + "<br>Location " + data.city + "," + data.region + "," + data.country_name
 
-        country.value = data.country_name
-        city.value = data.city
-        park.value = data.org
-        // ipAdress.value = data.ip
+            country.value = data.country_name
+            city.value = data.city
+            park.value = data.org
+            // ipAdress.value = data.ip
 
-        country.style.opacity = 0
-        city.style.opacity = 0
-        park.style.opacity = 0
-        country.style.width = "1px"
-        city.style.width = "1px"
-        park.style.width = "1px"
+            country.style.opacity = 0
+            city.style.opacity = 0
+            park.style.opacity = 0
+            country.style.width = "1px"
+            city.style.width = "1px"
+            park.style.width = "1px"
 
-        const pCountry = document.createElement("p")
-        pCountry.textContent = data.country_name
-        countryFilled.appendChild(pCountry)
+            const pCountry = document.createElement("p")
+            pCountry.textContent = data.country_name
+            countryFilled.appendChild(pCountry)
 
-        const pCity = document.createElement("p")
-        pCity.textContent = data.city
-        cityFilled.appendChild(pCity)
+            const pCity = document.createElement("p")
+            pCity.textContent = data.city
+            cityFilled.appendChild(pCity)
 
-        const pPark = document.createElement("p")
-        pPark.textContent = data.org
-        AtFilled.appendChild(pPark)
-    })
+            const pPark = document.createElement("p")
+            pPark.textContent = data.org
+            AtFilled.appendChild(pPark)
+        })
+    }
 }
