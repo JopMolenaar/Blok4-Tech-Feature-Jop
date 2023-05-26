85addf19a662750b92d085f97751360d206ab74d
Jop <jopmolenaar@icloud.com>
2023-05-26 21:52:09 +0200

    checkbox in filters

--- static/js/filter.js
+++ static/js/filter.js
@@ -5,6 +5,9 @@ window.onload = () => {
     const locationsList = document.querySelector("#locations ul")
     const selects = filterNav.querySelectorAll("select")
     const labels = filterNav.querySelectorAll("label")
+    const ckeckBox = filterNav.querySelectorAll("input[type=checkbox]")
+    // needs to be a fix where you can see if a box is till checked
+    console.log(ckeckBox)
     const jsOff = (document.getElementById("javascriptOff").style.display = "none")
     labels.forEach((label) => {
         label.style.display = "flex"
@@ -27,11 +30,33 @@ window.onload = () => {
             }
         })
     })
+    let isItEvenArray = []
+    ckeckBox.forEach((box) => {
+        box.addEventListener("change", () => {
+            isItEvenArray.push(box.value)
+            const filter = isItEvenArray.filter((word) => word === box.value)
+            const others = locationsList.querySelectorAll(`li`)
+            if (filter.length % 2 == 0) {
+                others.forEach((item) => {
+                    item.style.display = "grid"
+                })
+            } else {
+                //odd
+                const answers = locationsList.querySelectorAll(`.${box.value}`)
+                others.forEach((item) => {
+                    item.style.display = "none"
+                })
+                answers.forEach((item) => {
+                    item.style.display = "grid"
+                })
+            }
+        })
+    })
     let optionValueArray = []
     const fixOptions = () => {
         const options = filterNav.querySelectorAll("option")
         options.forEach((option) => {
-            if (optionValueArray.includes(`${option.value}`) || option.value === "") {
+            if ((optionValueArray.includes(`${option.value}`) && option.value != 0) || option.value === "") {
                 option.style.display = "none"
             }
             optionValueArray.push(`${option.value}`)
